58d5066ba1b82874d5d39ec2a257615e3f15c0d2
Francesco <f.guerrieri@gmail.com>
2010-04-23 21:24:22 +0200

    CMake sample

---
+++ src/sample.c
@@ -0,0 +1,18 @@
+// A simple program that computes the square root of a number
+// from http://www.cmake.org/cmake/help/cmake_tutorial.html
+#include <stdio.h>
+#include <math.h>
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+        {
+            fprintf(stdout,"Usage: %s number\n",argv[0]);
+            return 1;
+        }
+    double inputValue = atof(argv[1]);
+    double outputValue = sqrt(inputValue);
+    fprintf(stdout,"The square root of %g is %g\n",
+              inputValue, outputValue);
+    return 0;
+}
+
